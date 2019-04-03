651a21f829b7a19ec5d9d6675f29a05aa40dc57d
Sailendra Damaraju <42380691+SailendraDs@users.noreply.github.com>
2019-04-03 22:46:26 +0530

    Add files via upload
    Developed for 3rd party System

---
+++ first.php
@@ -0,0 +1,67 @@
+<?php
+include('header.php');
+include('titlehead1.php');
+?>
+
+<html>
+<head >
+
+<style>
+.hero{
+	background-image: url("https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?cs=srgb&dl=abstract-background-blur-255379.jpg&fm=jpg");
+	height:400px;
+	width:1575px;
+	background-position:center;
+	background-color:#cccccc;
+	position:relative;
+	background-size:cover;
+	background-repeat:no-repeat;
+}
+.button {
+
+ display: inline-block;
+  padding: 15px 25px;
+  font-size: 24px;
+  cursor: pointer;
+  text-align: center;
+  text-decoration: none;
+  outline: none;
+  color: #fff;
+  background-color: #4CAF50;
+  border: none;
+  border-radius: 15px;
+  box-shadow: 0 9px #999;
+}
+.wrapper {
+    text-align: center;
+}
+
+
+
+	
+</style>	
+</head>
+<body >
+
+<div class="hero">
+<a href="https://sdmcet.ac.in/">
+<h1>
+<p style="text-align:center;"><b>WELCOME</p><br>
+<br>
+
+<p style="text-align:center;"> <a href='addalumni.php'>CLICK HERE TO ADD DETAILS</a></p>
+</h1>
+</a>
+</div>
+<br>
+<h3>
+<p  style="text-align:center;"><b>Please give your valuable feedback.</b></p> </h3>
+<br>
+<div class="wrapper">
+
+<a href="php-email-form/formpage.html"  class="button">FEEDBACK</a><br><br><br>
+<h4 style="color:red"><b>NOTE: </b>Please use desktop  site in case of any difficuly.</h4>
+</div>
+</body>
+
+</html>
---
+++ header3.php
@@ -0,0 +1,10 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+	    <li><a href="first.php">Back To Home Page</a></li>
+      
+      </ul>
+    </div>
+  </div>
+</nav>
